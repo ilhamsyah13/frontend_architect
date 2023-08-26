7add5273373457782debc3a789cc316e6a7a0ea8
ilhamsyah13 <ilham1304syah@gmail.com>
2023-08-26 14:24:51 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
-  content: ["./src/**/*.{html,jsx}"],
+  content: ["./assets/**/*.{html,jsx}"],
   theme: {
     extend: {
       colors: {
