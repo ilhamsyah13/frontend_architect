593196927f576dbbc30536ef519081950686f995
ilhamsyah13 <ilham1304syah@gmail.com>
2023-08-26 13:53:26 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./frontend_architect",
+  base: "./frontend_architect/",
   plugins: [react()],
 });
